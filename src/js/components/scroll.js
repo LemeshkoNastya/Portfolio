bc4bc09b813cfce1ab74e2f3df02a6e5cc81988c
LemeshkoNastya <anastasia.lemeshko23@gmail.com>
2021-06-28 19:10:31 +0400

    fix: scroll

--- src/js/components/scroll.js
+++ src/js/components/scroll.js
@@ -7,28 +7,33 @@ const classHeader = '.header';
 const classMenuListLine = '.menu__list__line';
 const classMenuListLineActive = 'menu__list__line_active';
 const layoutMenuLineShadow = `<div class="menu__list__line__shadow"></div>`;
+const classMainButton = '.main__button';
 
 // Variables
 const headerListLink = document.querySelectorAll(classHeaderListLink);
 const menu = document.querySelectorAll(classMenuListLink);
-
 const sections = document.querySelectorAll(section);
 const header = document.querySelector(classHeader);
 const menuListLine = document.querySelectorAll(classMenuListLine);
+const mainButton = document.querySelector(classMainButton);
 
 // Functions
+const scrollElement = (element) => {
+    element.addEventListener('click', function (e) {
+        e.preventDefault();
+    
+        const href = this.getAttribute(attributeHref).substring(1);
+        const scrollTarget = document.getElementById(href);
+    
+        if (scrollTarget) {
+            scrollTarget.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+};
+
 const scrollSection = (listLink) => {
     listLink.forEach(link => {
-        link.addEventListener('click', function (e) {
-            e.preventDefault();
-
-            const href = this.getAttribute(attributeHref).substring(1);
-            const scrollTarget = document.getElementById(href);
-
-            if (scrollTarget) {
-                scrollTarget.scrollIntoView({ behavior: 'smooth' });
-            }
-        });
+        scrollElement(link);
     });
 };
 
@@ -36,7 +41,6 @@ window.addEventListener('scroll', () => {
     let scrollDistance = window.scrollY;
     
     sections.forEach((el, i) => {
-        
         if (el.offsetTop - header.clientHeight <= scrollDistance) {
             menuListLine.forEach(el => {
                 if (el.classList.contains(classMenuListLineActive)) {
@@ -44,6 +48,7 @@ window.addEventListener('scroll', () => {
                     el.innerHTML = '';
                 }
             })
+
             menuListLine[i].classList.add(classMenuListLineActive);
             menuListLine[i].innerHTML = layoutMenuLineShadow;
         }
@@ -52,3 +57,4 @@ window.addEventListener('scroll', () => {
 
 scrollSection(headerListLink);
 scrollSection(menu);
+scrollElement(mainButton);
